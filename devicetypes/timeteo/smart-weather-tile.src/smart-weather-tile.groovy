3dee93321450f74dd60fd56af24133e6e31638ac
Timeteo <timeteo@mypersonal.win>
2019-03-03 15:04:26 -0800

    trying things to get ST github integration to work :-(

--- devicetypes/timeteo/smart-weather-tile.src/smart-weather-tile.groovy
+++ devicetypes/timeteo/smart-weather-tile.src/smart-weather-tile.groovy
@@ -12,12 +12,12 @@
  *
  *  SmartWeather Station
  *
- *  Author: SmartThings
+ *  Author: SmartThings - modified by Timeteo
  *
  *  Date: 2013-04-30
  */
 metadata {
-    definition (name: "SmartWeather Station Tile", namespace: "timeteo", author: "Timeteo") {
+    definition (name: "SmartWeather Station Tile 3.0", namespace: "timeteo", author: "Timeteo") {
         capability "Illuminance Measurement"
         capability "Temperature Measurement"
         capability "Relative Humidity Measurement"
