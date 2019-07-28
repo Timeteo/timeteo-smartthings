091c9739e3344bb701fa0e19867b126113814146
Timeteo <timeteo@vemios.com>
2019-07-28 07:40:32 -0700

    Add inital release comment

--- devicetypes/timeteo/pentair-link2O-relay-switch.src/pentair-link2O-relay-switch.groovy
+++ devicetypes/timeteo/pentair-link2O-relay-switch.src/pentair-link2O-relay-switch.groovy
@@ -11,6 +11,10 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
+ *
+ *  7/28/2019 - Version 1.0.0
+ *              Initial release.
+ *
  */
 
 private getRootUrl()		{ "https://calc.mylink2o.com" }
